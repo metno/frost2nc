f5dd961de6347eaeea9cfddc1ced7f5ad46bb8aa
Vegard Bønes <vegard.bones@met.no>
2018-11-20 12:00:49 +0100

    update installation

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name='frost_extract',
     version='0.1',
-    packages=find_packages(),
+    # packages=find_packages(),
     install_requires=[
         'Click',
         'netCDF4',
         'python-dateutil',
         'cf_units',
-        'jinja2'
+        'jinja2',
+        'pyyaml',
     ],
+    packages=['frost_extract'],
+    package_dir={'frost_extract': 'frost_extract'},
     package_data={
-        'frost_extract': ['templates/mmd.xml', 'templates/global_attributes.xml']
+        'frost_extract': ['templates/mmd.xml', 'templates/global_attributes.yaml']
         },
     entry_points='''
         [console_scripts]
